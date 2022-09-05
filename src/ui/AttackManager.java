3d90ac11940bd7e1f17dc57ebf300bf15fbd5ceb
Michelle <mwang45@student.ubc.ca>
2022-09-05 01:17:41 -0700

    AttackManager started

--- src/ui/AttackManager.java
+++ src/ui/AttackManager.java
@@ -1,6 +1,19 @@
 package ui;
 
+import model.Player;
+
 public class AttackManager {
+    private Player opponent;
+    private Player challenger;
+
+
+    public AttackManager(Player player) {
+
+    }
+
+    public void createOpponent() {
+
+    }
 
 
 }
