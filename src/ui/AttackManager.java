f007d972ab797f77a3ffeec8649a23a8d0ff69d0
Michelle <mwang45@student.ubc.ca>
2022-09-06 00:24:54 -0700

    AttackManager started v2

--- src/ui/AttackManager.java
+++ src/ui/AttackManager.java
@@ -1,18 +1,26 @@
 package ui;
 
 import model.Player;
+import model.Pog;
 
 public class AttackManager {
     private Player opponent;
     private Player challenger;
 
+    private Pog fiona;
 
     public AttackManager(Player player) {
+        createOpponent();
+        System.out.println(challenger.getName() + " with " + fiona.getName() + " approaches");
+
 
     }
 
     public void createOpponent() {
+        challenger = new Player("Shrek");
+        fiona = new Pog("Fiona", 5);
 
+        challenger.addPog(fiona);
     }
 
 
